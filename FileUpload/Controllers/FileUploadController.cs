05c498c4998fb18812cb3650d84f91b8209886fe
Francis Elendu <elcfran6@hotmail.com>
2020-07-25 14:02:14 -0500

    Add project files.

---
+++ FileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUpload.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public FileUploadController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var fileuploadViewModel = await LoadAllFiles();
+            ViewBag.Message = TempData["Message"];
+            return View(fileuploadViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadToFileSystem(List<IFormFile> files, string description)
+        {
+            foreach (var file in files)
+            {
+                //Gets the base Path, i.e, The Current Directory of the application + /Files/. Feel free to change this to your choice.
+                var basePath = Path.Combine(Directory.GetCurrentDirectory() + "\\Files\\");
+
+                //Checks if the base path directory exists, else creates it.
+                bool basePathExists = System.IO.Directory.Exists(basePath);
+
+                if (!basePathExists) Directory.CreateDirectory(basePath);
+
+                //Gets the file name without the extension.
+               var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+
+                //Combines the base path with the file name.
+                var filePath = Path.Combine(basePath, file.FileName);
+
+                //Gets the extension of the file. (*.png, *.mp4, etc)
+                var extension = Path.GetExtension(file.FileName);
+                if (!System.IO.File.Exists(filePath))
+                {
+                    //If the file doesnt exist in the generated path, we use a filestream object, and create a new file, and then copy the contents to it.
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+
+                    //Create a new FileOnFileSystemModel object with required values.
+                    var fileModel = new FileOnFileSystemModel
+                    {
+                        CreatedOn = DateTime.UtcNow,
+                        FileType = file.ContentType,
+                        Extension = extension,
+                        Name = fileName,
+                        Description = description,
+                        FilePath = filePath
+                    };
+
+                    //Inserts this model to the db via the context instance of efcore.
+                    context.FilesOnFileSystem.Add(fileModel);
+                    context.SaveChanges();
+                }
+            }
+
+            //Sets a message in the TempData.
+            TempData["Message"] = "File successfully uploaded to File System.";
+
+            //Redirects to the Index Action Method.
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadToDatabase(List<IFormFile> files, string description)
+        {
+            foreach (var file in files)
+            {
+                //Gets the file name without the extension.
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+
+                //Gets the extension of the file. (*.png, *.mp4, etc)
+                var extension = Path.GetExtension(file.FileName);
+                var fileModel = new FileOnDatabaseModel
+                {
+                    CreatedOn = DateTime.UtcNow,
+                    FileType = file.ContentType,
+                    Extension = extension,
+                    Name = fileName,
+                    Description = description
+                };
+                //Creates a new MemoryStream object , convert file to memory object and appends ito our model’s object.
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    fileModel.Data = dataStream.ToArray();
+                }
+                context.FilesOnDatabase.Add(fileModel);
+                context.SaveChanges();
+            }
+            TempData["Message"] = "File successfully uploaded to Database";
+            return RedirectToAction("Index");
+        }
+
+        private async Task<FileUploadVM> LoadAllFiles()
+        {
+            var viewModel = new FileUploadVM();
+            viewModel.FilesOnDatabase = await context.FilesOnDatabase.ToListAsync();
+            viewModel.FilesOnFileSystem = await context.FilesOnFileSystem.ToListAsync();
+            return viewModel;
+        }
+
+        public async Task<IActionResult> DownloadFileFromDatabase(int id)
+        {
+
+            var file = await context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            return File(file.Data, file.FileType, file.Name + file.Extension);
+        }
+        public async Task<IActionResult> DownloadFileFromFileSystem(int id)
+        {
+
+            var file = await context.FilesOnFileSystem.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(file.FilePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, file.FileType, file.Name + file.Extension);
+        }
+        public async Task<IActionResult> DeleteFileFromFileSystem(int id)
+        {
+
+            var file = await context.FilesOnFileSystem.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            if (System.IO.File.Exists(file.FilePath))
+            {
+                System.IO.File.Delete(file.FilePath);
+            }
+            context.FilesOnFileSystem.Remove(file);
+            context.SaveChanges();
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from File System.";
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteFileFromDatabase(int id)
+        {
+
+            var file = await context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            context.FilesOnDatabase.Remove(file);
+            context.SaveChanges();
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from Database.";
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+    
+//}
+
---
+++ FileUpload/Models/FileUploadVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.Models
+{
+    public class FileUploadVM
+    {
+        public List<FileOnFileSystemModel> FilesOnFileSystem { get; set; } = new List<FileOnFileSystemModel>();
+        public List<FileOnDatabaseModel> FilesOnDatabase { get; set; } = new List<FileOnDatabaseModel>();
+    }
+}
